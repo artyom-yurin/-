66170a82ec4862a990f7bd1718c557bec8cefb46
Artem <artem_yrin@mail.ru>
2016-09-07 20:00:29 +0400

    Removed duplicating code

--- СиАОД/LW1.cpp
+++ СиАОД/LW1.cpp
@@ -8,14 +8,20 @@
 // jump height for every time point with step 0.1 seconds.
 // Program should print all time points when height is min and max.
 //
-// TODO: Fix negative value
 // TODO: Fix all warnings on high warning level (/W4, -Wall -Wextra).
 // TODO: Rename variables and split to several functions,
 // see also http://github.com/ps-group/ips-wiki/blob/master/prog_theory/coding-style/cpp-style-sfml.md
+
+void printResult(float g, float maxTimeLift, float currentTime)
+{
+	float initialVelocity = g * maxTimeLift;
+	float currentHeight = initialVelocity * currentTime - 0.5 * g * currentTime * currentTime;
+	printf("Current Time=%f, Current Height=%f\n", currentTime, currentHeight);
+}
+
 int main(int, char *[])
 {
 	const float g = 9.8f;
-	//float maxTimeLift;
 	int maxHeight;
 	printf("Max Height: ");
 	if (0 == scanf("%d", &maxHeight))
@@ -28,7 +34,7 @@ int main(int, char *[])
 	// v(currentTime) == v0 - g * currentTime
 	// v0 = g * maxTimeLift
 	// currentHeight(currentTime) == v0 * currentTime - 0.5 * g * currentTime * currentTime
-	const float maxTimeLift = sqrt(maxHeight * 2 / g);
+	float maxTimeLift = sqrt(maxHeight * 2 / g);
 	printf("Max Time Lift=%f\n", maxTimeLift);
 	bool doesMaxHeight = false;
 	for (float currentTime = 0; currentTime < maxTimeLift * 2; currentTime += 0.1f) //TODO: REPLACE FOR TO WHILE
@@ -36,18 +42,12 @@ int main(int, char *[])
 		if (currentTime > maxTimeLift && !doesMaxHeight)
 		{
 			doesMaxHeight = true;
-			float initialVelocity = g * maxTimeLift;
-			float currentHeight = initialVelocity * maxTimeLift - 0.5 * g * maxTimeLift * maxTimeLift;
-			printf("Current Time=%f, Current Height=%f\n", maxTimeLift, currentHeight);
+			printResult(g, maxTimeLift, maxTimeLift);
 		}
-		float initialVelocity = g * maxTimeLift;
-		float currentHeight = initialVelocity * currentTime - 0.5 * g * currentTime * currentTime;
-		printf("Current Time=%f, Current Height=%f\n", currentTime, currentHeight);
+		printResult(g, maxTimeLift, currentTime);
 	}
 
-	float initialVelocity = g * maxTimeLift;
-	float currentHeight = initialVelocity * (maxTimeLift * 2) - 0.5 * g * (maxTimeLift * 2) * (maxTimeLift * 2);
-	printf("Current Time=%f, Current Height=%f\n", maxTimeLift * 2, currentHeight);
+	printResult(g, maxTimeLift, maxTimeLift * 2);
 
 	return 0;
 }
