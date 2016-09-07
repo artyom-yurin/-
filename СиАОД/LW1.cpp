f24e52328230b047b5a4779311e2509fb96963fe
Artem <artem_yrin@mail.ru>
2016-09-07 23:02:00 +0400

    Revert "Added secure input function"
    This reverts commit e296e77568ab6df2f6eca9fa629fa70ffb8f948c.

--- СиАОД/LW1.cpp
+++ СиАОД/LW1.cpp
@@ -1,34 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+#include <vector>
 #include <cmath>
 
 // This program takes max jump height from input and prints
 // jump height for every time point with step 0.1 seconds.
 // Program should print all time points when height is min and max.
+//
+// TODO: Fix all warnings on high warning level (/W4, -Wall -Wextra).
+// TODO: Rename variables and split to several functions,
+// see also http://github.com/ps-group/ips-wiki/blob/master/prog_theory/coding-style/cpp-style-sfml.md
 
-void PrintResult(float g, float maxTimeLift, float currentTime)
+void printResult(float g, float maxTimeLift, float currentTime)
 {
 	float initialVelocity = g * maxTimeLift;
-	double currentHeight = initialVelocity * currentTime - 0.5 * g * currentTime * currentTime;
+	float currentHeight = initialVelocity * currentTime - 0.5 * g * currentTime * currentTime;
 	printf("Current Time=%f, Current Height=%f\n", currentTime, currentHeight);
-}
-safe
-int SafeScanf()
-{
-	int bufer = 0;
-	if (0 == scanf("%d", &bufer))
-	{
-		printf("\n" "expected floating-point number" "\n");
-		exit(1);
-	}
-	return bufer;
 }
 
 int main(int, char *[])
 {
 	const float g = 9.8f;
+	int maxHeight;
 	printf("Max Height: ");
-	int maxHeight = SafeScanf();
+	if (0 == scanf("%d", &maxHeight))
+	{
+		printf("\n" "expected floating-point number" "\n");
+		exit(1);
+	}
+	// maxTimeLift - time point when height is at maximum.
+	// currentTime - current time point
+	// v(currentTime) == v0 - g * currentTime
+	// v0 = g * maxTimeLift
+	// currentHeight(currentTime) == v0 * currentTime - 0.5 * g * currentTime * currentTime
 	float maxTimeLift = sqrt(maxHeight * 2 / g);
 	printf("Max Time Lift=%f\n", maxTimeLift);
 	bool doesMaxHeight = false;
@@ -38,13 +43,13 @@ int main(int, char *[])
 		if (currentTime > maxTimeLift && !doesMaxHeight)
 		{
 			doesMaxHeight = true;
-			PrintResult(g, maxTimeLift, maxTimeLift);
+			printResult(g, maxTimeLift, maxTimeLift);
 		}
-		PrintResult(g, maxTimeLift, currentTime);
+		printResult(g, maxTimeLift, currentTime);
 		currentTime += 0.1f;
 	}
 
-	PrintResult(g, maxTimeLift, maxTimeLift * 2);
+	printResult(g, maxTimeLift, maxTimeLift * 2);
 
 	return 0;
 }
